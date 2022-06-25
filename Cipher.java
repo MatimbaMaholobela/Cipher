3ea3f3604629e3e894d774030dbff92b5ac9fb46
Matimba Maholobela <mhlmat029@myuct.ac.za>
2022-06-25 22:22:49 +0200

    initial code

---
+++ Cipher.java
@@ -0,0 +1,15 @@
+class Cipher{
+
+	static String [] alpha  = "A, B, C, D, E, F, G, H, I, K, L, M, N, O, P, Q, R, S, T, V, X, Y, Z".split(",");
+
+	public static void main(String args []){
+		System.out.println(2%26);
+	}
+
+	public String  cipher(String msg, int k){
+
+
+
+	}
+
+}
