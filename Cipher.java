95b656e9a26607d99109aa96888f74478cf01e1a
Matimba Maholobela <mhlmat029@myuct.ac.za>
2022-06-25 23:10:00 +0200

    wrong input

--- Cipher.java
+++ Cipher.java
@@ -1,13 +1,38 @@
+import java.lang.Math;
 class Cipher{
 
-	static String [] alpha  = "A, B, C, D, E, F, G, H, I, K, L, M, N, O, P, Q, R, S, T, V, X, Y, Z".split(",");
+	static String [] alpha  = "A, B, C, D, E, F, G, H, I, K, L, M, N, O, P, Q, R, S, T, V, X, Y, Z".toLowerCase().split(",");
 
 	public static void main(String args []){
-		System.out.println(2%26);
+		System.out.println(decipher("DWWDFN CHUJ DW GDZQ",3));
 	}
 
-	public String  cipher(String msg, int k){
+	public static String  decipher(String msg, int k){
 
+		String toReturn = "";
+
+		for(int i=0;i<msg.length();i++){
+
+			if(msg.charAt(i)==(' ')){
+				toReturn += " ";
+			}
+			else{
+
+				int d = msg.charAt(i) -k;
+
+				if(d<0){  toReturn += alpha[alpha.length-1-Math.abs(d)];
+				}
+
+				else { toReturn += alpha[d];
+	
+				}
+
+			}
+
+	
+		}
+
+		return toReturn;
 
 
 	}
