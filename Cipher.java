ddd6d0b3dd9e776192a8bb932985995f7f7423c7
Matimba Maholobela <mhlmat029@myuct.ac.za>
2022-07-16 13:32:23 +0200

    cipher method added

--- Cipher.java
+++ Cipher.java
@@ -1,11 +1,11 @@
 
 class Cipher{
 
-	static String[] alpha  = "A, B, C, D, E, F, G, H, I, K, L, M, N, O, P, Q, R, S, T, V, X, Y, Z".split(",");
+	static String[] alpha  = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z".split(", ");
 	static int M = alpha.length;
 
 	public static void main(String args []){
-		System.out.println(cipher("Attack zerg at dawn",3));
+		System.out.println("message "+cipher("Attack zerg at dawn",3));
 	}
 
 	public static String cipher(String text, int k){
@@ -18,12 +18,14 @@ class Cipher{
 			
 			for(int j=0;j<M;j++){
 			
-				if(msg.charAt(i)==(' ')){
+				if(msg.substring(i,i+1).equals(" ")){
 					toReturn += " ";
+					break;
 				}
-				else if(msg.substring(i,i+1)==alpha[j].toLowerCase()) {
+				else if(msg.substring(i,i+1).equals(alpha[j].toLowerCase())) {
 				
 					toReturn+=alpha[(j+k)%M];
+
 				}
 			}
 
